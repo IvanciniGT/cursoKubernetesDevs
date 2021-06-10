3ce4d4723d14ca379c5e2a6c60f03574e8726712
Ubuntu <ubuntu@ip-172-31-6-211.eu-west-1.compute.internal>
2021-06-10 10:12:33 +0000

    es

--- instalacion/volumenes.sh
+++ instalacion/volumenes.sh
@@ -18,7 +18,7 @@ function generar-yaml-volumen (){
     echo "          type: DirectoryOrCreate"
 }
 
-volumenes="volumen1 volumen2 volumen3 volumen4 volumen5 volumen6 volumen7 volumen8 volumen9 volumen10"
+volumenes="es01 es02 es03 es04 es05 es06 es07 es08 es09 es10 es11 es12 es13 es14"
 for vol_name in $volumenes;
 do
     echo "$(generar-yaml-volumen $vol_name)"  | kubectl apply -f -
